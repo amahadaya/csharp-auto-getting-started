735d2db9584e12eb45294801ba1ab791325dfc17
Raymond Chi <rchi@vailsys.com>
2022-11-02 16:43:58 -0500

    Init csproj

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using freeclimb.Api;
+using freeclimb.Model;
+
+
+
+namespace GettingStarted.Controllers
+{
+    [Route("voice/")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // POST voice/
+        [HttpPost]
+        public string Post(CallStatusCallback request)
+        {
+          // Create a PerCl script
+          PerclScript helloScript = new PerclScript();
+
+          // Create a Say Command
+          Say sayHello = new Say();
+          sayHello.Text = "Hello, FreeClimb!";
+
+          // Add the command
+          helloScript.Commands.Add(sayHello);
+
+          // Respond to FreeClimb with your script
+          return helloScript.ToJson();
+        }
+    }
+}
